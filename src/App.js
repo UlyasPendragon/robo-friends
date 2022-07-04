22dd17f3d3f3ade7114103ddeb3967e9e004f23c
Curtis Reker <74525241+UlyasPendragon@users.noreply.github.com>
2022-07-03 20:13:20 -0400

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,11 @@
+//* App 
+
+import React from "react";
+import CardList from "./CardList";
+import { robots } from './robots';
+
+const App = () => {
+    return <CardList robots={robots} />
+}
+
+export default App;
